eb8d2cfa38499e36d69583640d709a821a54a7db
Sneha Tilak <sneha.tilak26@gmail.com>
2016-09-19 22:54:05 -0500

    Commiting POJO Class for Data Ingestor

---
+++ api-ingestor/src/com/ingestor/objects/URLObjects.java
@@ -0,0 +1,29 @@
+package com.ingestor.objects;
+
+import java.util.Date;
+
+public class URLObjects 
+{
+	private Date date;
+	private String stationName;
+	private String time;
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getStationName() {
+		return stationName;
+	}
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}	
+}
