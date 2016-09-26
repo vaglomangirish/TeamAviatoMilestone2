e2ad88eb3a2aee538bac75010b33a2682af4e3e3
Sneha Tilak <sneha.tilak26@gmail.com>
2016-09-26 19:08:51 -0400

    Updating path to Data Ingestor API

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 echo 'Installing the Data Ingestor API...' 
-cd '/home/ec2-user/DataIngestorAPI'
-sudo mvn -e clean install >> /var/log/DataIngestorAPI.log
+cd '/home/ec2-user/api-ingestor'
+sudo mvn -e clean install >> /var/log/api-ingestor.log
 cp reqDir/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
