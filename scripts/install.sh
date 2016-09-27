d514980f4efb7e7918e9c16060da31093a42c908
Amol Bhagwat <amolrbhagwat@gmail.com>
2016-09-27 18:34:47 -0400

    Removing previously installed node_modules

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,10 @@
 cd '/home/ec2-user/Gateway'
 
-echo "Installing the dependencies from package.json"
+#echo "Installing the dependencies from package.json"
 
 
 #echo "starting the application run weather forecast"
+
+rm -rf /home/ec2-user/Gateway/Gateway_Aviato/node_modules/*
 
 nodemon app.js >> debug.log 2>&1 &
