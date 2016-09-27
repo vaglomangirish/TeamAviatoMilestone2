c79a9d926dc7a03d3a5dd847fb88caaee1e1aaa4
Amol Bhagwat <amolrbhagwat@gmail.com>
2016-09-27 18:23:13 -0400

    Changed static public dir

--- Gateway_Aviato/app.js
+++ Gateway_Aviato/app.js
@@ -107,6 +107,7 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use(express.static('./public'));
 
 /**
  * Primary app routes.
