aaf850f6bebb5ce8f1efe6a950846522fd70c069
Prajna <prajnacheryl@yahoo.co.in>
2016-09-27 01:53:30 -0400

    Updating folder name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 echo 'Installing the Registry API...' 
-cd '/home/ec2-user/registry'
+cd '/home/ec2-user/registryAPI/'
 mvn -e clean install >> /var/log/tomcat.log
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 sudo mvn -e clean install >> /var/log/registry.log
