180a2b10937fa5c2cb3a463e7d7ab15090e4a5fa
Prajna <prajnacheryl@yahoo.co.in>
2016-09-26 22:27:04 -0400

    Updated deploy and builf files for registry

---
+++ scripts/install.sh
@@ -0,0 +1,6 @@
+echo 'Installing the Registry API...' 
+cd '/home/ec2-user/registry'
+sudo mvn -e clean install >> /var/log/registry.log
+cp reqDir/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
+cd  /usr/local/tomcat7/apache-tomcat-7.0.72
+sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
