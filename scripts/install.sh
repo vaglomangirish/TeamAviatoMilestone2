5db542cef96d182aafb1cec6404b5bd898913b66
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-09-27 19:38:39 -0400

    Added linux commands to remove previous files

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,12 @@
 echo 'starting installation process' >> /var/log/sga-teamaviato-StormDetector-install.log
 cd '/home/ec2-user/stormdetection'
 
+rm -r /home/ec2-user/stormDetector
+mv /home/ec2-user/StormDetector  /home/ec2-user/stormDetector
+cd /home/ec2-user/stormDetector/
+chmod 777 stormdetection
+cd stormdetection
+
 echo 'Activating virtualenv for StormDetector Microservice' >> /var/log/sga-teamaviato-StormDetector-install.log
 pip install virtualenv >> /var/log/sga-teamaviato-StormDetector-install.log
 virtualenv env >> /var/log/sga-teamaviato-StormDetector-install.log
@@ -9,6 +15,6 @@ pip install Flask >> /var/log/sga-teamaviato-StormDetector-install.log
 pip install nose >> /var/log/sga-teamaviato-StormDetector-install.log
 pip install BeautifulSoup4 >> /var/log/sga-teamaviato-StormDetector-install.log
 pip install Flask-SQLAlchemy >> /var/log/sga-teamaviato-StormDetector-install.log
-#echo 'Running Flask Server' >> /var/log/sga-teamaviato-StormDetector-install.log
+echo 'Running Flask Server' >> /var/log/sga-teamaviato-StormDetector-install.log
 export FLASK_APP=stormdetector.py
-flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-teamaviato-StormDetector-server.log 2>&1 &
+flask run --host=127.0.0.1 --port=8000 >> /var/log/sga-teamaviato-StormDetector-server.log 2>&1 &
