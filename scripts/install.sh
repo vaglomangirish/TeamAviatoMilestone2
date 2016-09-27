339183f4be025ace830b1ac1194e28dc95127d4b
Prajna <prajnacheryl@yahoo.co.in>
2016-09-27 00:54:11 -0400

    Updating the install.sh file

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 echo 'Installing the Registry API...' 
 cd '/home/ec2-user/registry'
+mvn -e clean install >> /var/log/tomcat.log
+mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 sudo mvn -e clean install >> /var/log/registry.log
-cp reqDir/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
+cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
