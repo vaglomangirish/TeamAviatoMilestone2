d30594cde8bc23a2e60c5f79574dea7b1df07f5d
Gourav Shenoy <shenoy.200@gmail.com>
2016-09-27 23:52:04 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,8 @@
-cd '/home/ec2-user/Gateway'
+# copy files from Gateway to a new folder
+mkdir '/home/ec2-user/gateway-microservice'
+cp -r '/home/ec2-user/Gateway/*' '/home/ec2-user/gateway-microservice'
+
+#cd '/home/ec2-user/Gateway'
 
 #echo "Installing the dependencies from package.json"
 
@@ -6,5 +10,5 @@ cd '/home/ec2-user/Gateway'
 #echo "starting the application run weather forecast"
 
 
-
+cd '/home/ec2-user/gateway-microservice/Gateway_Aviato'
 nodemon app.js >> debug.log 2>&1 &
