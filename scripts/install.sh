49a242989dbd45c6baa73d3186c474241f0bc413
Sneha Tilak <sneha.tilak26@gmail.com>
2016-09-27 22:31:36 -0500

    Edited install.sh #19

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 echo 'Installing the Data Ingestor API...'
 cd '/home/ec2-user/api-ingestor/api-ingestor'
 mvn clean install >> /var/log/tomcat.log
+mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
+sudo mvn clean install >> /var/log/registry.log
 cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
