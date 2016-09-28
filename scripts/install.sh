add74ac9df7c6665300ce884cac0d21ed18b198a
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-09-27 20:34:41 -0400

    Added linux commands to remove previous files

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,11 @@
 echo 'starting installation process' >> /var/log/sga-teamaviato-StormClustering-install.log
-cd '/home/ec2-user/stormclustering'
+#cd '/home/ec2-user/stormclustering'
+
+rm -r /home/ec2-user/stormClustering
+mv /home/ec2-user/StormClustering  /home/ec2-user/stormClustering
+cd /home/ec2-user/stormClustering/
+chmod 777 stormclustering
+cd stormclustering
 
 echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/sga-teamaviato-StormClustering-install.log
 pip install virtualenv >> /var/log/sga-teamaviato-StormClustering-install.log
@@ -11,4 +17,4 @@ pip install BeautifulSoup4 >> /var/log/sga-teamaviato-StormClustering-install.lo
 pip install Flask-SQLAlchemy >> /var/log/sga-teamaviato-StormClustering-install.log
 echo 'Running Flask Server' >> /var/log/sga-teamaviato-StormClustering-install.log
 export FLASK_APP=stormclustering.py
-flask run --host=0.0.0.0 --port=64000 >> /var/log/sga-teamaviato-StormClustering-server.log 2>&1 &
+flask run --host=127.0.0.1 --port=31000 >> /var/log/sga-teamaviato-StormClustering-server.log 2>&1 &
