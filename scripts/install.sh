e9d4336bf8b57f8919e96fd89b799981f5d9085d
Prajna <prajnacheryl@yahoo.co.in>
2016-09-28 00:41:34 -0400

    Copying artifacts to a new directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,13 @@
+# copy artifacts to new dir
+mkdir '/home/ec2-user/registry-microservice'
+cp -r /home/ec2-user/registry/* /home/ec2-user/registry-microservice
+
+# delete revision
+rm -rf '/home/ec2-user/registry'
+
 echo 'Installing the Registry API...' 
-cd '/home/ec2-user/registry/registry/'
+cd '/home/ec2-user/registry-microservice/registry/'
+
 mvn clean install >> /var/log/tomcat.log
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 sudo mvn clean install >> /var/log/registry.log
