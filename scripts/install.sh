f3dd6c88fd08633a9afe3d9b4ee3b5cba3f1003f
Prajna <prajnacheryl@yahoo.co.in>
2016-09-27 21:35:29 -0400

    Updating mvn command

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 echo 'Installing the Registry API...' 
 cd '/home/ec2-user/registryAPI/'
-mvn -e clean install >> /var/log/tomcat.log
+mvn clean install >> /var/log/tomcat.log
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
-sudo mvn -e clean install >> /var/log/registry.log
+sudo mvn clean install >> /var/log/registry.log
 cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
