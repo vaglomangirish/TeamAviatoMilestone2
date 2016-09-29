4b77c5d6b70640628078702b2272ce78119a3492
Sneha Tilak <sneha.tilak26@gmail.com>
2016-09-29 17:23:14 -0500

    Changed IP. Fixing...

--- api-ingestor/src/main/java/com/sciencegateway/dataingestor/resource/RESTResource.java
+++ api-ingestor/src/main/java/com/sciencegateway/dataingestor/resource/RESTResource.java
@@ -55,16 +55,7 @@ public class RESTResource
 			jsonObject.put("url", urlObjects.getUrl());	
 			System.out.println(jsonObject.toString());
 			
-			ClientConfig clientConfigU = new ClientConfig();
-			Client clientU = ClientBuilder.newClient(clientConfigU);
-			clientU.property(ClientProperties.CONNECT_TIMEOUT, 5000);
-			WebTarget targetU = clientU.target("http://127.0.0.1:8080/dataingestor/webapi/service/log");
-			System.out.println(targetU.toString());
-			Response responseToU = targetU.request().post(Entity.entity(jsonObject.toString(), "application/json"),Response.class);
-			System.out.println(responseToU.toString());
-			responseToU.close();
-			
-			//generateLOG(urlObjects);
+			generateLOG(urlObjects);
 			return Response.ok(jsonObject.toString(), MediaType.APPLICATION_JSON).build();
 		} 
 		catch (MalformedURLException | ParseException exception)
@@ -78,9 +69,6 @@ public class RESTResource
 		}
 	}
 	
-	@POST
-	@Path("/log")
-	@Produces(MediaType.TEXT_PLAIN)
 	public void generateLOG(URLObjects urlObjects) throws Exception
 	{
 		System.out.println("Sending LOG to Registry...");
@@ -93,7 +81,7 @@ public class RESTResource
 		ClientConfig clientConfigR = new ClientConfig();
 		Client clientR = ClientBuilder.newClient(clientConfigR);
 		clientR.property(ClientProperties.CONNECT_TIMEOUT, 5000);
-		WebTarget targetR = clientR.target("http://127.0.0.1:8080/registry/v1/service/log");
+		WebTarget targetR = clientR.target("http://localhost:8080/registry/v1/service/log");
 		System.out.println(targetR.toString());
 		Response responseToR = targetR.request().post(Entity.entity(jsonObject.toString(), "application/json"),Response.class);
 		System.out.println(responseToR.toString());
