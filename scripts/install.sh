f25f31be07f525939ba412a90f98cd5568f8e846
Sneha Tilak <sneha.tilak26@gmail.com>
2016-09-28 20:57:14 -0400

    Updating tomcat server details Fixes #24

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,8 +9,7 @@ echo 'Installing the Data Ingestor API...'
 cd '/home/ec2-user/ingestor-microservice/api-ingestor'
 
 mvn clean install >> /var/log/tomcat.log
-mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
-sudo mvn clean install >> /var/log/registry.log
 cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
+
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
