4200223584dd0b3517acea0dada0d48786251661
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-09-29 00:31:36 -0400

    changed host to 0.0.0.0 from 127.0.0.1

--- runforecast/runforecast.py
+++ runforecast/runforecast.py
@@ -45,4 +45,4 @@ def get_forecast(cluster):
 
     return {'weatherType':weatherType,'temperature':temperature,'units':units}
 if __name__ == '__main__':
-    app.run(port=8050)
+    app.run(host='0.0.0.0',port=8050)
