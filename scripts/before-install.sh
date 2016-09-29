902d68a690c5e91bf6cd457ca75c0671326807dd
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-09-28 22:11:26 -0400

    modified kill process

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,4 +1,3 @@
-echo 'killing existing flask process if any'
-sudo touch /var/log/teamaviato.log
 
-ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/teamaviato.log 2>&1 &
+echo 'killing existing flask process if any'
+sudo kill -9 $(sudo lsof -i :32000 | grep LISTEN) >> processkill.log 2>&1 &
