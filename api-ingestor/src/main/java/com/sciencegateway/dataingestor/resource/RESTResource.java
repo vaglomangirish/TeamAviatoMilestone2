089c1929d62339b881733bfb72777ad46a538b83
Sneha Tilak <sneha.tilak26@gmail.com>
2016-09-29 01:54:36 -0500

    Changed IP. Works...

--- api-ingestor/src/main/java/com/sciencegateway/dataingestor/resource/RESTResource.java
+++ api-ingestor/src/main/java/com/sciencegateway/dataingestor/resource/RESTResource.java
@@ -80,7 +80,7 @@ public class RESTResource
 		ClientConfig clientConfigR = new ClientConfig();
 		Client clientR = ClientBuilder.newClient(clientConfigR);
 		clientR.property(ClientProperties.CONNECT_TIMEOUT, 5000);
-		WebTarget targetR = clientR.target("http://127.0.0.1:8080/registry/v1/service/log");
+		WebTarget targetR = clientR.target("http://localhost:8080/registry/v1/service/log");
 		System.out.println(targetR.toString());
 		Response responseToR = targetR.request().post(Entity.entity(jsonObject.toString(), "application/json"),Response.class);
 		System.out.println(responseToR.toString());
