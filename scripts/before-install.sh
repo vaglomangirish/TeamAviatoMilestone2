e66032ff8a0ef122adaddf5a4d81297dbf723f70
Prajna <prajnacheryl@yahoo.co.in>
2016-09-28 21:59:00 -0400

    Updated tomcat install steps

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,22 +1,28 @@
-echo 'killing existing tomcat process if any'
-cd /usr/local/tomcat7/apache-tomcat-7.0.72
-sudo sh ./bin/shutdown.sh
-sleep 20
-
 export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk.x86_64
 
 echo 'check if tomcat is installed'
 cd /usr/local/tomcat7/apache-tomcat-7.0.72 
-./bin/version.sh
+sudo ./bin/version.sh
 if [ "$?" -ne 0 ]; then
    echo 'Installing Tomcat...'
    	sudo wget http://www.us.apache.org/dist/tomcat/tomcat-7/v7.0.72/bin/apache-tomcat-7.0.72.tar.gz
-	tar xzf apache-tomcat-7.0.68.tar.gz
-	sudo mv apache-tomcat-7.0.68 /usr/local/tomcat7
+	tar xzf apache-tomcat-7.0.72.tar.gz
+	sudo mkdir /usr/local/tomcat7
+	sudo mv apache-tomcat-7.0.72 /usr/local/tomcat7
 	cd /usr/local/tomcat7/apache-tomcat-7.0.72
-	./bin/startup.sh
-	./bin/version.sh
+	sudo ./bin/startup.sh
+	sudo ./bin/version.sh
+else
+   echo 'Killing existing tomcat process if any'
+	cd /usr/local/tomcat7/apache-tomcat-7.0.72
+	sudo sh ./bin/shutdown.sh
+	sleep 20
 fi
+
+echo 'killing existing tomcat process if any'
+cd /usr/local/tomcat7/apache-tomcat-7.0.72
+sudo sh ./bin/shutdown.sh
+sleep 20
 
 echo 'check if maven is installed'
 mvn --version
