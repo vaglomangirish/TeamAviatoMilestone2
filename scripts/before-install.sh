9c9c45d0ea1126c8c375bb86fe1d3d3528588ab4
Prajna <prajnacheryl@yahoo.co.in>
2016-09-28 22:48:25 -0400

    updating tomcat installation

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -19,11 +19,6 @@ else
 	sleep 20
 fi
 
-echo 'killing existing tomcat process if any'
-cd /usr/local/tomcat7/apache-tomcat-7.0.72
-sudo sh ./bin/shutdown.sh
-sleep 20
-
 echo 'check if maven is installed'
 mvn --version
 if [ "$?" -ne 0 ]; then
