351f3ba8b9ca5443f0ab36cfb71c9584100c0017
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-10-20 02:15:19 -0400

    added unit tests

--- stormclustering/test_stormclustering.py
+++ stormclustering/test_stormclustering.py
@@ -1,8 +1,13 @@
 from unittest import TestCase
-
+from stormclustering import app
 
 class TestPerformclustering(TestCase):
     def test_performclstering(self):
         from stormclustering import performclustering
         self.assertTrue(type(performclustering('<xmldata>')) == dict)
+
+    def test_stormclustering_endpoint(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
 
