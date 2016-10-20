1a3645cf51d49ad1676bb89418b21ec1821a5d8b
Sneha Tilak <sneha.tilak26@gmail.com>
2016-10-20 02:56:50 -0500

    Adding java files #29

--- api-ingestor/src/test/java/com/sciencegateway/dataingestor/POJO/URLObjectsTest.java
+++ api-ingestor/src/test/java/com/sciencegateway/dataingestor/POJO/URLObjectsTest.java
@@ -1,13 +1,6 @@
 package com.sciencegateway.dataingestor.POJO;
-import org.junit.Test;
 
 public class URLObjectsTest
 {
-	
-	@Test
-	public void testPOJO() throws Exception
-	{
-		new BeanTester().testBean(URLObjects.class);
-	}
 	
 }
