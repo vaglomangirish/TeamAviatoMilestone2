a31c927502ea498aedd65bf33570f55f17475805
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-10-21 17:57:37 -0400

    Added unit and component test cases

--- stormdetection/test_storm_detector.py
+++ stormdetection/test_storm_detector.py
@@ -18,6 +18,7 @@ class TestSendkml(unittest.TestCase):
         response=connect.get("/stormdetector/v1/service/noaa-nexrad-level2.s3.amazonaws.com/03/12/12/ST/abc.txt")
         self.assertEqual(response.status_code, 200)'''
 
+#component test
 class FlaskTestCase(unittest.TestCase):
     # test runforecast endpoint
     def test_stormdetector_endpoint(self):
