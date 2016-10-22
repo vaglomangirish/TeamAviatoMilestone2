61d2550e9f0bdb391c95ea2de1fb3efb1242b08d
Sneha Tilak <sneha.tilak26@gmail.com>
2016-10-21 23:35:20 -0400

    Updated install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,5 +14,7 @@ cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
 
+cd '/home/ec2-user/ingestor-microservice/api-ingestor'
+
 docker build -t dataingestor . >> /var/log/dockerlog.log 2>&1 &
 docker run -d -p 8888:8080 --name api-di dataingestor >> /var/log/dataingestor.log 2>&1 &
