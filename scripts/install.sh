0af393d2f784d4d67804487ad9c2530f366740fe
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-10-21 21:11:28 -0400

    Changed docker image name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 echo 'Building Docker' >> /var/log/sdetector-docker.log 2>&1
-docker build --build-arg APP_URL=http://s3-us-west-2.amazonaws.com/teamaviatobucket2/aviatoStormDetector.tar.gz -t aviato/python:v1 . >> /var/log/sdetector-docker.log 2>&1
+docker build --build-arg APP_URL=http://s3-us-west-2.amazonaws.com/teamaviatobucket2/aviatoStormDetector.tar.gz -t sdetect_img . >> /var/log/sdetector-docker.log 2>&1
 echo 'Running docker' >> /var/log/sdetector-docker.log 2>&1
-docker run -it --name storm-detector-service -h aviato.python -p 8000:8000 -d aviato/python:v1
+docker run -it --name storm-detector-service -h aviato.python -p 8000:8000 -d sdetect_img
