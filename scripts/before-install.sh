4e89f3ee9b3463258e68113519870b6352245be3
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-10-22 01:08:09 -0400

    Added working Docker commands

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,2 +1,6 @@
-echo 'killing existing flask process if any'
-sudo kill -9 $(sudo lsof -i :32000 | grep LISTEN) >> processkill.log 2>&1 &
+echo 'killing existing process on port 32000 if any'
+sudo kill -9 $(sudo lsof -i :32000 | grep LISTEN) >> /var/log/processkill.log 2>&1 &
+
+echo 'Removing existing docker instances' >> /var/log/ftrigger-docker.log 2>&1
+docker ps -a | grep 'ftrigger_img' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+docker ps -a | grep 'ftrigger_img' | awk '{print $1}' | xargs --no-run-if-empty docker rm
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-echo 'starting installation process' >> /var/log/sga-teamaviato-StormClustering-install.log
+echo 'starting installation process' >> /var/log/sga-teamaviato-ForecastTrigger-install.log
 #cd '/home/ec2-user/forecasttrigger'
 
 rm -r /home/ec2-user/forecastTrigger
@@ -7,16 +7,5 @@ cd /home/ec2-user/forecastTrigger/
 chmod 777 forecasttrigger
 cd forecasttrigger
 
-echo 'Activating virtualenv for Forecast Trigger Microservice' >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-pip install virtualenv >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-virtualenv env >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-source env/bin/activate >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-pip install Flask >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-pip install nose >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-pip install BeautifulSoup4 >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-pip install Flask-SQLAlchemy >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-pip install ConfigParser
-pip install requests
-
-echo 'Running Flask Server for forecast_trigger' >> /var/log/sga-teamaviato-ForecastTrigger-install.log
-nohup python forecast_trigger.py > /dev/null 2>&1 &
+docker build -t ftrigger_img .
+docker run -d -p 32000:32000 --name api-ftrigger ftrigger_img >> sga-teamaviato-ForecastTrigger-docker-server.log 2>&1 &
