b644308047e4bb988f5cc8b01cfaba46bf13ade8
Laxmi Bhavani Malkareddy <laxmibh.malkareddy@gmail.com>
2016-10-22 04:46:25 -0400

    Added bridge network

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,10 @@ cd /home/ec2-user/stormDetector/
 chmod 777 stormdetection
 cd stormdetection
 
+#setup bridge network
+docker network create --subnet=172.18.0.0/16 mynet123
+if [ $? == 0 ]; then    echo "created subnet"; 
+else echo "subnet already present"; fi
 
 docker build -t sdetect_img .
 docker run -d --net mynet123 --ip 172.18.0.31 -p 8000:8000 --name api-sdetect sdetect_img >> sga-teamaviato-StormDetector-docker-server.log 2>&1 &
