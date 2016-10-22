d5cece80af111b441037253f762159ce5392b5ea
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-10-21 23:45:03 -0400

    Added commands to kill process on port 8000

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,3 +1,6 @@
+echo 'killing existing process on port 8000 if any'
+sudo kill -9 $(sudo lsof -i :8000 | grep LISTEN) >> /var/log/processkill.log 2>&1 &
+
 echo 'Removing existing docker instances' >> /var/log/sdetector-docker.log 2>&1
 docker ps -a | grep 'sdetect_img' | awk '{print $1}' | xargs --no-run-if-empty docker stop
 docker ps -a | grep 'sdetect_img' | awk '{print $1}' | xargs --no-run-if-empty docker rm
