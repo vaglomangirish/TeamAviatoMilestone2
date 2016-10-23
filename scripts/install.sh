45452a904f33ece0e53763acd1ab07ec296b66dd
Laxmi Bhavani Malkareddy <laxmibh.malkareddy@gmail.com>
2016-10-23 17:15:37 -0400

    Added subnet

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,11 +7,11 @@ cd /home/ec2-user/runForecast/
 chmod 777 runforecast
 cd runforecast
 
-#setup bridge network
-#docker network ls | grep 'mynet123'
-#if [ $? ne 0 ]; then
-#  docker network create --subnet=172.18.0.0/16 mynet123
-#fi
+setup bridge network
+docker network ls | grep 'mynet123'
+if [ $? ne 0 ]; then
+  docker network create --subnet=172.18.0.0/16 mynet123
+fi
 
 docker build -t runforecast_img .
-docker run -d -p 8050:8050 --name api-runforecast runforecast_img >> sga-teamaviato-RUnForecast-docker-server.log 2>&1 &
+docker run -d --net mynet123 --ip 172.18.0.34 -p 8050:8050 --name api-runforecast runforecast_img >> sga-teamaviato-RUnForecast-docker-server.log 2>&1 &
