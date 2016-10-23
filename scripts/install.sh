f3170a6acf97b53c7f2fea4cd34b2a6d464e98ab
Laxmi Bhavani Malkareddy <laxmibh.malkareddy@gmail.com>
2016-10-23 15:04:15 -0400

    Set docker container to subnet

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,13 +7,13 @@ cd /home/ec2-user/stormDetector/
 chmod 777 stormdetection
 cd stormdetection
 
-#setup bridge network
-#docker network ls | grep 'mynet123'
-#if [ $? ne 0 ]; then
-#  docker network create --subnet=172.18.0.0/16 mynet123
-#fi
+setup bridge network
+docker network ls | grep 'mynet123'
+if [ $? ne 0 ]; then
+  docker network create --subnet=172.18.0.0/16 mynet123
+fi
 
 docker build -t sdetect_img .
-docker run -d --net="host" -p 8000:8000 --name api-sdetect sdetect_img >> sga-teamaviato-StormDetector-docker-server.log 2>&1 &
+docker run -d --net mynet123 --ip 172.18.0.31 -p 8000:8000 --name api-sdetect sdetect_img >> sga-teamaviato-StormDetector-docker-server.log 2>&1 &
 
 
