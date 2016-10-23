7295ddf50c865dda0316a750499c9e2481e6b55d
Laxmi Bhavani Malkareddy <laxmibh.malkareddy@gmail.com>
2016-10-23 00:19:49 -0400

    Changed docker network to connect to localhost

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,11 +8,11 @@ chmod 777 forecasttrigger
 cd forecasttrigger
 
 #setup bridge network
-docker network ls | grep 'mynet123'
-if [ $? ne 0 ]; then
-  docker network create --subnet=172.18.0.0/16 mynet123
-fi
+#docker network ls | grep 'mynet123'
+#if [ $? ne 0 ]; then
+#  docker network create --subnet=172.18.0.0/16 mynet123
+#fi
 
 docker build -t ftrigger_img .
 #docker run -d -p 32000:32000 --name api-ftrigger ftrigger_img >> sga-teamaviato-ForecastTrigger-docker-server.log 2>&1 &
-docker run -d --net mynet123 --ip 172.18.0.33 -p 32000:32000 --name api-ftrigger ftrigger_img >> sga-teamaviato-ForecastTrigger-docker-server.log 2>&1 &
+docker run -d --net="host" -p 32000:32000 --name api-ftrigger ftrigger_img >> sga-teamaviato-ForecastTrigger-docker-server.log 2>&1 &
