d35cf4935f074870b395a6f6dbde94ddcd009761
Amol Bhagwat <amolrbhagwat@gmail.com>
2016-10-23 18:45:15 -0400

    Testing a different external port

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,7 +19,7 @@ docker build -t gateway . > /var/log/gatewayimagebuild.log 2>&1 &
 docker images | grep '<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f 2>&1 > /var/log/gatewayrmi.log 2>&1
 #docker rmi -f $(docker images | grep '^<none>' | awk '{print $3}') >> /var/log/dockergatewayimage.log 2>&1 &
 docker ps -a | grep 'api-g' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f 2>&1 > /var/log/gatewayrm.log 
-docker run -d -p 3000:3000 --name api-g --net=host gateway 2>&1 > /var/log/gateway.log
+docker run -d -p 4000:3000 --name api-g --net=host gateway 2>&1 > /var/log/gateway.log
 
 #echo "Fixing node-sass"
 
