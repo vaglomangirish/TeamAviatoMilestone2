572954a6cec52dabfeff63402b3734651b94fb7f
Laxmi Bhavani Malkareddy <laxmibh.malkareddy@gmail.com>
2016-10-23 15:07:59 -0400

    Add docker network subnet

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,11 +7,11 @@ cd /home/ec2-user/stormClustering/
 chmod 777 stormclustering
 cd stormclustering
 
-#setup bridge network
-#docker network ls | grep 'mynet123'
-#if [ $? ne 0 ]; then
-#  docker network create --subnet=172.18.0.0/16 mynet123
-#fi
+setup bridge network
+docker network ls | grep 'mynet123'
+if [ $? ne 0 ]; then
+  docker network create --subnet=172.18.0.0/16 mynet123
+fi
 
 docker build -t scluster_img .
-docker run -d --net="host" -p 31000:31000 --name api-sclustering scluster_img >> sga-teamaviato-StormClustering-docker-server.log 2>&1 &
+docker run -d --net mynet123 --ip 172.18.0.32 -p 31000:31000 --name api-sclustering scluster_img >> sga-teamaviato-StormClustering-docker-server.log 2>&1 &
