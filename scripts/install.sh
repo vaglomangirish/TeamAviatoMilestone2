b7d9bedeeb64831cf74dae9ef50a53082179ecac
Sneha Tilak <sneha.tilak26@gmail.com>
2016-10-24 01:03:19 -0400

    Changed port binding

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,4 +21,4 @@ cd '/home/ec2-user/docker'
 docker login -e="sneha.tilak26@gmail.com" -u="tilaks" -p="teamAviato"
 docker pull tilaks/dataingestor
 docker images | grep '<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f
-docker run -d -p 9000:8080 --name api-di $(docker images | grep -w "tilaks/dataingestor" | awk '{print $3}') >> /var/log/dataingestor.log 2>&1 &
+docker run -d -p 9000:8009 --name api-di $(docker images | grep -w "tilaks/dataingestor" | awk '{print $3}') >> /var/log/dataingestor.log 2>&1 &
