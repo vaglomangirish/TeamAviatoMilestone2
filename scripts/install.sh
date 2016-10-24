9c0324ef653adab181316e14de1a8e5b0cabd2ea
Sneha Tilak <sneha.tilak26@gmail.com>
2016-10-23 20:25:55 -0400

    Changed grep command

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,4 +21,4 @@ cd '/home/ec2-user/docker'
 docker login -e="sneha.tilak26@gmail.com" -u="tilaks" -p="teamAviato"
 docker pull tilaks/dataingestor
 docker images | grep '<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f
-docker run -d -p 11000:9000 --name api-di --net=host $(sudo docker images | grep tilaks/dataingestor | awk '{print $3}') >> /var/log/dataingestor.log 2>&1 &
+docker run -d -p 11000:9000 --name api-di --net=host $(docker images | grep -w "tilaks/dataingestor" | awk '{print $3}') >> /var/log/dataingestor.log 2>&1 &
