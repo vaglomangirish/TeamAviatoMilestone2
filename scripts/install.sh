338e0d478bd6c9bd1d652fbfb6449dba533d5c16
Laxmi Bhavani Malkareddy <laxmibh.malkareddy@gmail.com>
2016-10-24 19:04:55 -0400

    Added log to docker build command

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,7 @@ if [ $? ne 0 ]; then
   docker network create --subnet=172.18.0.0/16 mynet123
 fi
 
-docker build -t sdetect_img_test . >>sDetect-test-img-build.log
+docker build -t sdetect_img_test . >> sDetect-test-img-build.log
 docker run -d --net mynet123 --ip 172.18.0.41 -p 9000:9000 --name api-sdetect-test sdetect_img_test >> sga-teamaviato-StormDetector-test-docker-server.log 2>&1 &
 
 
