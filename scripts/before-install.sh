5865e506faf244818eb2a795be1efca823723eba
Laxmi Bhavani Malkareddy <laxmibh.malkareddy@gmail.com>
2016-10-24 19:11:07 -0400

    Changed port to 9000

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,5 +1,5 @@
-echo 'killing existing process on port 8000 if any'
-sudo kill -9 $(sudo lsof -i :9000 | grep LISTEN) >> /var/log/processkill-test.log 2>&1 &
+echo 'killing existing process on port 9500 if any'
+sudo kill -9 $(sudo lsof -i :9500 | grep LISTEN) >> /var/log/processkill-test.log 2>&1 &
 
 echo 'Removing existing docker instances' >> /var/log/sdetector-test-docker.log 2>&1
 docker ps -a | grep 'sdetect_img_test' | awk '{print $1}' | xargs --no-run-if-empty docker stop
